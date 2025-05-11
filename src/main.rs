7ea1b3976a3ef5cb97df4d28f005e3c5d1a078cb
elnosh <elnosh@pm.me>
2025-05-11 11:59:34 -0500

    commitment tx info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use bitcoin::{BlockHash, Network, bip32::Xpriv, constants::ChainHash};
+use bitcoin::{
+    Address, BlockHash, FeeRate, Network, bip32::Xpriv, constants::ChainHash, params::Params,
+};
 use ldk_node::{
     config::WALLET_KEYS_SEED_LEN,
     io::sqlite_store::{KV_TABLE_NAME, SQLITE_DB_FILE_NAME, SqliteStore},
@@ -18,7 +20,7 @@ use lightning::{
     },
 };
 use rand::{RngCore, thread_rng};
-use std::{env, fs, path::Path, sync::Arc};
+use std::{env, fs, path::Path, process, sync::Arc};
 use std::{io::Write, time::SystemTime};
 
 fn read_or_generate_seed_file(
@@ -46,11 +48,8 @@ fn read_or_generate_seed_file(
         }
 
         let mut f = fs::File::create(keys_seed_path)?;
-
         f.write_all(&key)?;
-
         f.sync_all()?;
-
         Ok(key)
     }
 }
@@ -63,7 +62,13 @@ impl Logger for NLogger {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let ldk_dir = args.get(1).unwrap();
+    let ldk_dir = match args.get(1) {
+        Some(dir) => dir,
+        None => {
+            println!("specify directory where ldk-node storage is");
+            process::exit(1);
+        }
+    };
 
     let ldk_store = SqliteStore::new(
         ldk_dir.into(),
@@ -94,9 +99,9 @@ fn main() {
 
     let mut reader = Cursor::new(res);
 
+    // stuff to read before reading the channel count
     let _v: u8 = Readable::read(&mut reader).unwrap();
     let _v: u8 = Readable::read(&mut reader).unwrap();
-
     let _c: ChainHash = Readable::read(&mut reader).unwrap();
     let _bh: u32 = Readable::read(&mut reader).unwrap();
     let _bh: BlockHash = Readable::read(&mut reader).unwrap();
@@ -120,19 +125,135 @@ fn main() {
 
         let logger = Arc::new(NLogger {});
 
+        println!(
+            "---------------------------------------------------------------------------------------------------"
+        );
+        println!("Channel ID: {}", chan.context.channel_id);
+        println!(
+            "Channel Size (sats): {}\n",
+            chan.funding.get_value_satoshis()
+        );
+
+        // funding tx
+        let funding_tx = chan.funding.funding_transaction.clone().unwrap();
+        println!("Funding Transaction: ");
+        println!("Transaction ID: {}", funding_tx.compute_txid());
+        println!("Inputs:");
+        for (index, input) in funding_tx.input.iter().enumerate() {
+            println!("  Input {}: {:?}", index, input.previous_output);
+        }
+        println!("\nOutputs:");
+        for (index, output) in funding_tx.output.iter().enumerate() {
+            println!("  Output {}:", index);
+            println!("    Value: {} satoshis", output.value);
+            println!("    Script PubKey: {}", output.script_pubkey);
+            println!(
+                "    Address: {}\n",
+                Address::from_script(&output.script_pubkey, Params::new(Network::Regtest)).unwrap()
+            );
+        }
+
         let current_commitment = chan.context.build_commitment_transaction(
             &chan.funding,
             chan.holder_commitment_point.transaction_number(),
             &chan.holder_commitment_point.current_point(),
-            true,
-            true,
+            chan.funding.is_outbound(),
+            false,
             &logger,
         );
-        println!("channel {}", chan.context.channel_id);
+
+        let pending_outgoing: u64 = current_commitment
+            .htlcs_included
+            .iter()
+            .filter(|htlc| htlc.0.offered)
+            .map(|htlc| htlc.0.amount_msat / 1000)
+            .sum();
+
+        let local_commitment_tx = current_commitment.tx;
+        let local_balance = local_commitment_tx.to_broadcaster_value_sat();
+        let remote_balance = local_commitment_tx.to_countersignatory_value_sat();
+
+        println!("Channel Information: ");
+        let (to_remote_reserve, to_self_reserve) = chan
+            .funding
+            .get_holder_counterparty_selected_channel_reserve_satoshis();
+        println!("Channel Reserve: {}", to_self_reserve.unwrap());
+
+        println!("Inbound Capacity: {}", (remote_balance - to_remote_reserve));
+        println!(
+            "Outbound Capacity: {}",
+            (local_balance - to_self_reserve.unwrap())
+        );
+
+        println!("Pending Outgoing HTLCs Amount: {}", pending_outgoing);
+
+        // info about the current commitment tx
+        println!("\nCurrent Commitment Transaction: ");
 
         println!(
-            "commitment tx {:?}",
-            current_commitment.tx.built.transaction
-        )
+            "Balance to local node: {}",
+            local_commitment_tx.to_broadcaster_value_sat()
+        );
+        println!(
+            "Balance to remote node: {}",
+            local_commitment_tx.to_countersignatory_value_sat()
+        );
+
+        println!(
+            "Feerate: {} sat/vB",
+            FeeRate::from_sat_per_kwu(local_commitment_tx.feerate_per_kw.into())
+                .to_sat_per_vb_ceil()
+        );
+        println!(
+            "Transaction fee: {}\n",
+            current_commitment.stats.total_fee_sat
+        );
+
+        let htlc_idxs: Vec<u32> = current_commitment
+            .htlcs_included
+            .iter()
+            .filter_map(|htlc| {
+                if htlc.0.transaction_output_index.is_some() {
+                    Some(htlc.0.transaction_output_index.unwrap())
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        println!("Inputs:");
+        for (index, input) in local_commitment_tx
+            .built
+            .transaction
+            .input
+            .iter()
+            .enumerate()
+        {
+            println!("  Input {}: {:?}", index, input.previous_output);
+        }
+        println!("\nOutputs:");
+        for (index, output) in local_commitment_tx
+            .built
+            .transaction
+            .output
+            .iter()
+            .enumerate()
+        {
+            if htlc_idxs.contains(&(index as u32)) {
+                println!("  Output {} (HTLC output):", index);
+            } else {
+                println!("  Output {}:", index);
+            }
+            println!("    Value: {} sats", output.value);
+            println!("    Script Pubkey: {}", output.script_pubkey);
+            println!(
+                "    Address: {}\n",
+                Address::from_script(&output.script_pubkey, Params::new(Network::Regtest)).unwrap()
+            );
+        }
+
+        println!(
+            "---------------------------------------------------------------------------------------------------"
+        );
     }
 }
